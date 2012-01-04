a18c775c4b860098f311ced460d645db99ddcf57
Grad van Horck <grad@forwart.nl>
2012-01-04 15:05:54 +0100

    Added support code for ioMenuPlugin

--- lib/helper/BootstrapHelper.php
+++ lib/helper/BootstrapHelper.php
@@ -23,3 +23,96 @@ function dv_bootstrap_breadcrumbs($nodes, $divider = '/') {
   $html .= '</ul>';
   return $html;
 }
+
+function render_bootstrap_menu(ioMenuItem $menu, $depth = null, $renderAsChild = false) {
+    if (!$menu->hasChildren() || $depth === 0 || !$menu->showChildren())
+    {
+      return;
+    }
+    
+    $attributes = $menu->getAttributes();
+    if (!isset($attributes['class'])) {
+        if ($renderAsChild) {
+            $attributes['class'] = 'dropdown-menu';
+        } else {
+            $attributes['class'] = 'nav';
+        }
+    }
+    
+    $html = '<ul'._tag_options($attributes).'>';
+    $childDepth = ($depth === null) ? null : ($depth - 1);
+    $html .= _render_bootstrap_menu_children($menu, $childDepth);
+    $html .= '</ul>';
+    
+    return $html;
+}
+
+function _render_bootstrap_menu_children(ioMenuItem $item, $depth = null) {
+    
+    $html = '';
+    foreach ($item->getChildren() as $child)
+    {
+      $html .= _render_bootstrap_menu_child($child, $depth);
+    }
+    return $html;
+    
+}
+
+function _render_bootstrap_menu_child(ioMenuItem $item, $depth = null) {
+    
+    // if we don't have access or this item is marked to not be shown
+    if (!$item->shouldBeRendered())
+    {
+      return; 
+    }
+
+    // explode the class string into an array of classes
+    $class = ($item->getAttribute('class')) ? explode(' ', $item->getAttribute('class')) : array();
+
+    if ($item->isCurrent())
+    {
+      $class[] = 'active';
+    }
+    elseif ($item->isCurrentAncestor($depth))
+    {
+      $class[] = 'current_ancestor';
+    }
+
+    if ($item->actsLikeFirst())
+    {
+      $class[] = 'first';
+    }
+    if ($item->actsLikeLast())
+    {
+      $class[] = 'last';
+    }
+    
+    // retrieve the attributes and put the final class string back on it
+    $attributes = $item->getAttributes();
+    
+    if ($item->hasChildren()) {
+        $item->setLinkOptions(array('class' => 'dropdown-toggle'));
+        $class[] = 'dropdown';
+        $attributes['data-dropdown'] = 'dropdown';  
+    }
+
+    if (count($class) > 0)
+    {
+      $attributes['class'] = implode(' ', $class);
+    }
+
+    // opening li tag
+    $html = '<li'._tag_options($attributes).'>';
+
+    // render the text/link inside the li tag
+    $html .= (null !== $item->getRoute()) ? $item->renderLink() : $item->renderLabel();
+
+    // renders the embedded ul if there are visible children
+    $html .= render_bootstrap_menu($item, $depth, true);
+
+    // closing li tag
+    $html .= '</li>';
+
+    return $html;
+    
+}
